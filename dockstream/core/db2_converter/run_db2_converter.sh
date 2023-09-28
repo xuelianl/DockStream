725ed75e8882c7340644b5cdeb63b699ab7f8562
xli02@k146 <xli02@k146.hn.org>
2023-09-28 11:01:07 +0800

    For DOCK3.7: update and use python version of db2_converter.     for faster, see https://github.com/hnlab/db2_converter/pull/17.

--- dockstream/core/db2_converter/run_db2_converter.sh
+++ dockstream/core/db2_converter/run_db2_converter.sh
@@ -2,10 +2,33 @@
 
 line=$1
 max_conf=$2
-sampletp=$3
-checkstereo=$4
-useff=$5
+workingpath=$3
+method=$4
+checkstereo=$5
+useff=$6
+sampletp=$7
 
+source /pubhome/xli02/opt/miniconda/etc/profile.d/conda.sh
+conda activate basic
 name=`echo $line|awk '{print $2}'|awk -F ':' '{print $1}'`
 echo $line > $name.smi
-/pubhome/xli02/git/db2_converter/ligand/generate/build_ligand.sh $name $max_conf $sampletp $checkstereo $useff &> conformator_$name.log
+# /pubhome/xli02/scripts/db2_converter/ligand/generate/build_ligand.sh $name $max_conf $sampletp $checkstereo $useff &> conformator_$name.log
+command="build_ligand -i $name.smi -m $max_conf --workingpath $workingpath --outputpath $workingpath --method $method"
+if [ $checkstereo == 'True' ];
+then
+    command+=" -c"
+fi
+
+if [ $useff == 'True' ];
+then
+    command+=" -f"
+fi
+
+if [ $sampletp == 'True' ];
+then
+    command+=" -s"
+fi
+
+# echo $command
+$command
+conda deactivate &> db2_${method}_$name.log
